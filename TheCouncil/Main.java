bfa38a4a41b60cd59053a8a1ca8fdbe818852aab
matthewyam <78432671+matthewyam@users.noreply.github.com>
2022-11-08 03:49:38 +0000

    add basic tests into main to test Members file

---
+++ TheCouncil/Main.java
@@ -0,0 +1,13 @@
+package TheCouncil;
+import java.io.IOException;
+
+
+public class Main{
+    
+    public static void main(String[] args) throws IOException{
+
+    Members one = new Members ("Miyuki", "Shirogane", "President", 12, 4);
+
+    System.out.print(one);
+    }
+}
