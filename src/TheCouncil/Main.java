91ef4c1ae76db220bcb75bdd811a0bab9b004b33
matthewyam <78432671+matthewyam@users.noreply.github.com>
2022-11-09 16:00:33 +0000

    sttarted on userinput

--- src/TheCouncil/Main.java
+++ src/TheCouncil/Main.java
@@ -1,10 +1,15 @@
 package TheCouncil;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 
 public class Main{
     
     public static void main(String[] args) throws IOException{
+
+        System.out.println("Welcome to the Student Council");
+        System.out.println("");
+        System.out.println("Enter your Role");
 
     President one = new President ("Miyuki", "Shirogane", "President", 12, 4, 10);
     VicePresident two = new VicePresident ("Kaguya", "Shinomiya", "Vice President", 12, 3, 1900749.00);
